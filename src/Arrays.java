8ec46f6f3790441a343651391ee126d459246ae2
Duy <duy.leduc8@gmail.com>
2026-01-16 17:53:38 +0700

    20260116

---
+++ src/Arrays.java
@@ -0,0 +1,20 @@
+//Arrays (the bedrock)
+//Primitive arrays
+//- int[], char[], long[], double[]
+//- Multidimensional: int[][], char[][]
+//When they appear
+//- Two pointers
+//- Sliding window
+//- Prefix sums
+//- Binary search
+//- DP tables
+//- Matrix traversal
+//- Key tricks
+//- Index math > elegance
+//- Pre-allocating for O(1) access
+//- In-place mutation to save space
+//Arrays are fast, dumb, and brutally effective.
+
+public class Arrays {
+    
+}
